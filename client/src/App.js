55587639ca3bd65347b1bc41b9b95472dd07e38d
xenacro <kayush054@gmail.com>
2021-01-28 23:00:40 +0530

    Profile Creation frontend and backend integration

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
@@ -32,6 +33,7 @@ const App = () => {
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
             <PrivateRoute exact path='/dashboard' component={Dashboard} />
+            <PrivateRoute exact path='/create-profile' component={CreateProfile} />
           </Switch>
         </section>
       </Fragment>
