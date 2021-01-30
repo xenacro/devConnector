f80fd288b5ad64857c49221682b4bfbec086e7df
xenacro <kayush054@gmail.com>
2021-01-31 00:49:27 +0530

    GITHUB repo request added

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE } from '../actions/types';
+import { GET_PROFILE, GET_PROFILES, GET_REPOS, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE } from '../actions/types';
 
 const initialState = {
     profile: null,
@@ -15,6 +15,10 @@ export default function(state = initialState, action) {
         case GET_PROFILE:
         case UPDATE_PROFILE:
             return { ...state, profile: payload, loading: false };
+        case GET_PROFILES:
+            return { ...state, profiles: payload, loading: false };
+        case GET_REPOS:
+            return { ...state, repos: payload, loading: false };
         case PROFILE_ERROR:
             return { ...state, error: payload, loading: false };
         case CLEAR_PROFILE:
