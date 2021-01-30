0583771d30c522f83b96616a14dc339ecb324f29
xenacro <kayush054@gmail.com>
2021-01-31 01:24:30 +0530

    Developers Profile request functionality

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
+import Profiles from './components/profiles/Profiles';
 import PrivateRoute from './components/routing/PrivateRoute';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
@@ -35,6 +36,7 @@ const App = () => {
           <Switch>
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
+            <Route exact path='/profiles' component={Profiles} />
             <PrivateRoute exact path='/dashboard' component={Dashboard} />
             <PrivateRoute exact path='/create-profile' component={CreateProfile} />
             <PrivateRoute exact path='/edit-profile' component={EditProfile} />
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -7,13 +7,14 @@ import { logout } from '../../actions/auth';
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     const authLinks = (
         <ul>
+            <li><Link to="/profiles">Developers</Link></li>
             <li><Link to="/dashboard"><i className='fas fa-user' />{' '}<span className='hide-sm'>Dashboard</span></Link></li>
             <li><a onClick={logout} href="#!1"><i className='fas fa-sign-out-alt' />{' '}<span className='hide-sm'>Logout</span></a></li>
         </ul>
     );
     const guestLinks = (
         <ul>
-                <li><a href="#!">Developers</a></li>
+                <li><Link to="/profiles">Developers</Link></li>
                 <li><Link to="/register">Register</Link></li>
                 <li><Link to="/login">Login</Link></li>
             </ul>
