77a1f23b6646e0338042029db4f97dd01d66c1b4
xenacro <kayush054@gmail.com>
2021-01-31 19:17:06 +0530

    Developer's Github repos added for visitors

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,6 +5,9 @@ import { Link } from 'react-router-dom';
 import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
@@ -20,6 +23,23 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
                     <div class="profile-grid my-1">
                         <ProfileTop profile={profile} />
                         <ProfileAbout profile={profile} />
+                        <div className="profile-exp bg-white p-2">
+                            <h2 className="text-primary">Experience</h2>
+                            {profile.experience.length > 0 ? (<Fragment>
+                                {profile.experience.map(experience => (
+                                    <ProfileExperience key={experience._id} experience={experience} />
+                                ))}
+                            </Fragment>) : (<h4>No experience credentials..</h4>)}
+                        </div>
+                        <div className="profile-edu bg-white p-2">
+                            <h2 className="text-primary">Education</h2>
+                            {profile.education.length > 0 ? (<Fragment>
+                                {profile.education.map(education => (
+                                    <ProfileEducation key={education._id} education={education} />
+                                ))}
+                            </Fragment>) : (<h4>No education credentials..</h4>)}
+                        </div>
+                        {profile.githubusername && (<ProfileGithub username={profile.githubusername} />)}
                     </div>
                 {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (
                 <Link to='/edit-profile' className='btn btn-dark'>Edit Profile</Link>
